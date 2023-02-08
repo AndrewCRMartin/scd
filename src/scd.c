047f9ca444d3c9f7f9aa0eaa23398bdd8abba511
AndrewCRMartin <andrew@bioinf.org.uk>
2023-02-08 12:16:52 +0000

    Added skeleton C code

---
+++ src/scd.c
@@ -0,0 +1,73 @@
+/************************************************************************/
+/**
+
+   Program:    scd
+   \file       scd.c
+   
+   \version    V0.1
+   \date       08.02.23  
+   \brief      Score Conservation through Diversity
+   
+   \copyright  (c) UCL / Prof. Andrew C. R. Martin 2023
+   \author     Prof. Andrew C. R. Martin
+   \par
+               Institute of Structural & Molecular Biology,
+               Division of Biosciences,
+               University College,
+               Gower Street,
+               London.
+               WC1E 6BT.
+   \par
+               andrew@bioinf.org.uk
+               andrew.martin@ucl.ac.uk
+               
+**************************************************************************
+
+   This program is not in the public domain, but it may be copied
+   according to the conditions laid out in the accompanying file
+   COPYING.DOC
+
+   The code may be modified as required, but any modifications must be
+   documented so that the person responsible can be identified.
+
+   The code may not be sold commercially or included as part of a 
+   commercial product except as described in the file COPYING.DOC.
+
+**************************************************************************
+
+   Description:
+   ============
+
+**************************************************************************
+
+   Usage:
+   ======
+
+**************************************************************************
+
+   Revision History:
+   =================
+   V0.1    03.02.23  Original  By: ACRM
+
+*************************************************************************/
+/* Includes
+*/
+#include <stdio.h>
+
+/************************************************************************/
+/* Defines and macros
+*/
+
+/************************************************************************/
+/* Globals
+*/
+
+/************************************************************************/
+/* Prototypes
+*/
+
+/************************************************************************/
+int main(int argc, char **argv)
+{
+   
+}
